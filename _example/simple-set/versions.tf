8f823af998af20f933f4776979dba6086f9f36ec
anmolnagpal <anmol@clouddrove.com>
2024-01-04 19:08:29 +0530

    feat: update github-action version and added automerge file

--- _example/simple-set/versions.tf
+++ _example/simple-set/versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.6.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.1.15"
+      version = ">= 5.31.0"
     }
   }
 }
