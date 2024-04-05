beb24aa66578bc9cd43f9b39c44e48f253b6e035
test <aatish.sharma@clouddrove.com>
2024-04-05 13:10:54 +0530

    fix: create multiple route53 records

--- variables.tf
+++ variables.tf
@@ -13,29 +13,6 @@ variable "record_alias_enabled" {
   description = "Whether to create Route53 alias record set."
 }
 
-variable "type" {
-  type        = string
-  default     = ""
-  description = "The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR, NS, PTR, SOA, SPF, SRV and TXT. "
-}
-
-variable "ttl" {
-  type        = number
-  default     = null
-  description = "(Required for non-alias records) The TTL of the record."
-}
-
-variable "name" {
-  type        = string
-  default     = ""
-  description = "The name of the record."
-}
-
-variable "values" {
-  type        = string
-  default     = ""
-  description = "(Required for non-alias records) A string list of records. To specify a single record value longer than 255 characters such as a TXT record for DKIM, add \"\" inside the Terraform configuration string (e.g. \"first255characters\"\"morecharacters\")."
-}
 
 variable "set_identifier" {
   type        = string
@@ -47,12 +24,6 @@ variable "health_check_id" {
   type        = string
   default     = ""
   description = "The health check the record should be associated with."
-}
-
-variable "alias" {
-  type        = map(any)
-  default     = {}
-  description = "An alias block. Conflicts with ttl & records. Alias record documented below."
 }
 
 variable "multivalue_answer_routing_policy" {
@@ -67,10 +38,6 @@ variable "allow_overwrite" {
   description = "Allow creation of this record in Terraform to overwrite an existing record, if any. This does not affect the ability to update the record in Terraform and does not prevent other resources within Terraform or manual Route 53 changes outside Terraform from overwriting this record. false by default. This configuration is not recommended for most environments."
 }
 
-variable "zone_id" {
-  type        = string
-  description = "Zone ID."
-}
 
 variable "records" {
   description = "Specifies values for route53 private alias records"
